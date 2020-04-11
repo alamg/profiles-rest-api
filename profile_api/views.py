aeb0a5c27dd8c37bdd65c7f68a891a010ea6478d
Alamgir Hossain <alamg9078@gmail.com>
2020-04-10 23:42:41 -0700

    Added search filter

--- profile_api/views.py
+++ profile_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 from profile_api import serializer
 from profile_api import models
@@ -89,3 +91,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes =(permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
